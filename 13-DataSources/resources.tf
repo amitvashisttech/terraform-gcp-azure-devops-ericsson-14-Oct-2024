0afbf09ca943611638f64c7ce747f9518449ef2b
Amit Vashist <amitvashist.tech@gmail.com>
2024-10-16 07:17:07 +0000

    SSH

--- 13-DataSources/resources.tf
+++ 13-DataSources/resources.tf
@@ -1,15 +1,13 @@
 
 resource "google_compute_instance" "west_frontend" {
   depends_on   = [ google_compute_instance.west_backend ]
-  #name         = join("-", tolist([var.environment-name, "frontend-west"]))
   name         = local.west_frontend_name
   count        = var.multi-region-deployment ? 1 : 0
-  #zone         = var.us-west-zones[count.index]
   zone         = data.google_compute_zones.us-west1.names[count.index]
   machine_type = var.instance_type
   boot_disk {
     initialize_params {
-      image = var.boot_disk_image
+      image = data.google_compute_image.my_image.self_link
     }
   }
   network_interface {
@@ -22,16 +20,14 @@ resource "google_compute_instance" "west_frontend" {
 
 resource "google_compute_instance" "frontend" {
   provider     = google.myprovider2
-  #name         = join("-", tolist([var.environment-name, "frontend"]))
   name  = local.default_frontend_name
   depends_on   = [ google_compute_instance.backend ]
   count        = 1
-  #zone         = var.us-central-zones[count.index]
   zone     		= data.google_compute_zones.us-central1.names[count.index]
   machine_type = var.instance_type
   boot_disk {
     initialize_params {
-      image = var.boot_disk_image
+      image = data.google_compute_image.my_image.self_link
     }
   }
   network_interface {
@@ -45,15 +41,13 @@ resource "google_compute_instance" "frontend" {
 
 resource "google_compute_instance" "backend" {
   provider     = google.myprovider2
-  #name         = join("-", tolist([var.environment-name, "backend"]))
   name  = local.default_backend_name
   machine_type = var.instance_type
   count        = 1
-  #zone         = var.us-central-zones[count.index]
   zone         = data.google_compute_zones.us-central1.names[count.index]
   boot_disk {
     initialize_params {
-      image = var.boot_disk_image
+      image = data.google_compute_image.my_image.self_link
     }
   }
   network_interface {
@@ -70,10 +64,8 @@ resource "google_compute_instance" "backend" {
 
 resource "google_compute_instance" "west_backend" {
   machine_type = var.instance_type
-  #name         = join("-", tolist([var.environment-name, "backend-west"]))
   name         = local.west_backend_name
   count        = var.multi-region-deployment ? 2 : 0
-  #zone         = var.us-west-zones[count.index]
   zone         = data.google_compute_zones.us-west1.names[count.index]
   boot_disk {
     initialize_params {
--- 13-DataSources/variable.tf
+++ 13-DataSources/variable.tf
@@ -1,23 +1,9 @@
-variable "us-west-zones" {
-  default = ["us-west1-a", "us-west1-b"]
-}
-
-variable "us-central-zones" {
-  default = ["us-central1-a", "us-central1-b"]
-}
-
 variable "multi-region-deployment" {
   default = true
 }
 
 variable "environment-name" {
   default = "tfdemo"
-}
-
-variable "boot_disk_image" {
-  type        = string
-  default     = "debian-cloud/debian-12"
-  description = "Varible for Instance Boot Image"
 }
 
 variable "instance_type" {
